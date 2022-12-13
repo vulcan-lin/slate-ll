159ea0eb0369a592e7228dc15dc34f5ea73ec98d
shuaikang.lin <shuaikang.lin@xtalpi.com>
2022-12-13 09:25:07 +0800

    git pull before build
    Signed-off-by: shuaikang.lin <shuaikang.lin@xtalpi.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-widdershins --search false  --summary swagger.json -o source/index.html.md
 git pull
+widdershins --search false  --summary swagger.json -o source/index.html.md
 git add .
 git commit -m "update"
 git push
